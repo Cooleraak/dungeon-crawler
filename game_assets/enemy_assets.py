738ca5d84df8950227cbc5405344af8b83a0decc
Jakub Davidov <36138874@cuni.cz>
2024-11-05 21:42:38 +0100

    Fix minor iteration issues

--- game_assets/enemy_assets.py
+++ game_assets/enemy_assets.py
@@ -1,74 +1,76 @@
+class enemy_tiers():
+	def __init__(self):
+		self.basic_enemy_list = ['anaconda','crab','cow','crow','demon_small','goblin']
+		self.elite_enemy_list = ['bear','demon_big','skeleton','angel','spider']
+		self.boss_enemy_list = ['ouroboros','dragon','ophanim','devil']
+
 class enemy_action_properties():
 	def __init__(self):
-		self.basic_enemy_action_properties = {
-			'anaconda':('teeth','poison'),
-			'crab':('claws'),
-			'cow':('teeth','weigth'),
-			'crow':('claws'),
+		self.enemy_actions = {
+			'anaconda':['teeth'],
+			'crab':['claws'],
+			'cow':('teeth','weight'),
+			'crow':['claws'],
 			'demon_small':('claws','teeth','fire'),
 			'goblin':('teeth','claws'),
-			}
-
-		self.elite_enemy_action_properties = {
-			'bear':('teeth','claws','weigth'),
+			'bear':('teeth','claws','weight'),
 			'demon_big':('claws','fire_big'),
-			'skeleton':('regenerative','punch'),
-			'angel':('regenerative','elusive','fire'),
-			'spider':('poison','teeth','elusive'),
+			'skeleton':['punch'],
+			'angel':['fire'],
+			'spider':['teeth'],
+			'ouroboros':['teeth'],
+			'dragon':('teeth','claws','fire_big','weight'),
+			'ophanim':('weight','fire_big'),
+			'devil':('claws','fire_big'),
 			}
 
-		self.boss_enemy_action_properties = {
-			'ouroboros':('regenerative','teeth','poison'),
-			'dragon':('teeth','claws','fire_big','weight'),
-			'ophanim':('elusive','weight','fire_big'),
-			'devil':('claws','fire_big','summoner_devil'),
+		self.enemy_passive_actions = {
+			'anaconda':['poison'],
+			'crow':['elusive'],
+			'skeleton':['regenerative'],
+			'angel':('regenerative','elusive'),
+			'spider':('poison','elusive'),
+			'ouroboros':('regenerative','poison'),
+			'ophanim':['elusive'],
+			'devil':['summoner_devil'],
 			}
 
 class enemy_stat_properties():
 	def __init__(self):
-		self.basic_enemy_action_properties = {
-			'anaconda':(100, 5),
-			'crab':(50, 10),
-			'cow':(150, 3),
-			'crow':(30, 10),
-			'demon_small':(70, 10),
-			'goblin':(70, 5),
-			}
-
-		self.elite_enemy_action_properties = {
-			'bear':(150, 15),
-			'demon_big':(120, 20),
-			'skeleton':(70, 20),
-			'angel':(80, 15),
-			'spider':(90, 15),
-			}
-
-		self.boss_enemy_action_properties = {
-			'ouroboros':(500, 30),
-			'dragon':(500, 30),
-			'ophanim':(300, 40),
-			'devil':(300, 40),
+		# 'enemy_name' : (hp, dmg_multiplier),
+		self.enemy_stats = {
+			'anaconda':(1000, 5),
+			'crab':(500, 6),
+			'cow':(1500, 3),
+			'crow':(300, 6),
+			'demon_small':(700, 6),
+			'goblin':(700, 5),
+			'bear':(1500, 8),
+			'demon_big':(1200, 9),
+			'skeleton':(700, 9),
+			'angel':(800, 8),
+			'spider':(900, 8),
+			'ouroboros':(5000, 11),
+			'dragon':(5000, 11),
+			'ophanim':(3000, 12),
+			'devil':(3000, 12),
 			}
 
 class enemy_attacks():
-	def __init__(self):
-		self.basic_attacks = {
-			'claws':(),
-			'teeth':(),
-			'weight':(),
-			}
-
-		self.basic_skills = {
-			'fire':(),
-			'poison':(),
-			}
-
-		self.elite_skills = {
-			'fire_big':(),
-			'summoner_devil':(),
-			}
-
-		self.passive_skills = {			
-			'regenerative':(),
-			'elusive':(),
+	def __init__(self): 
+		# 'move' : (dmg_raw)
+		self.attacks_and_skills = {
+			'claws':10,
+			'teeth':12,
+			'weight':14,
+			
+		# 'move' : (tick_rate)
+			'fire':1,
+			'fire_big':3,
+			'summoner_devil':3,
+			
+		# 'passive' : (potency)
+			'regenerative':3,
+			'elusive':2,
+			'poison':1,
 			}
