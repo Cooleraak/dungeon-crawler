357dd37a41c571a7971fd23b9e6f827adbe0ed63
Jakub Davidov <36138874@cuni.cz>
2025-01-14 17:55:03 +0100

    Add xp progression and adjust player strength

--- game_assets/player.py
+++ game_assets/player.py
@@ -1,35 +1,59 @@
-#ADD INVENTORY FOR PLAYER
-
 class player():
-    def __init__(self):
-        data = construct_player_data()
-        level, moveset, passives = data.curr_level(), data.curr_moveset(), data.curr_passives() 
-
+    type = 'PLAYER'
+    def __init__(self, level = 1, moveset = ['punch', 'punch'], passives = ['poison'], item = 3, curr_xp = 0):
+        self.level = level
         self.name = "player"
-        self.hp = 100*level
-        self.dmg_multiplier = 2*level
-        self.actions = moveset
+        self.hp = 550*level
+        self.max_hp = 550*level
+        self.dmg_multiplier = 30*level
+        self.mp = 100
+        self.actions = ['pass'] + moveset 
         self.passives = passives
-        self.inventory = {'050potion':0,
-                          '100potion':0,
-                          '500potion':0,
-            }
-        self.curr_xp = 0
+        self.inventory = 5
+        self.curr_xp = curr_xp
         self.curr_max_xp = 100*level
+
+    def update_xp(self):
+        self.curr_xp += 20
+        if self.curr_xp>= self.curr_max_xp:
+            self.level += 1 if self.level<6 else 0
+            self.curr_xp = 0
+            self.curr_max_xp = 120*self.level
+            self.actions =  construct_player_data().curr_moveset(self.level)
+            self.passives = construct_player_data().curr_passives(self.level)
 
 
 class save_data():
+    #save player data into a file
     def __init__(self, player):
-        pass #save player data into file
+        with open('game_assets\save.txt','w') as file:
+            file.write(f'{player.level} {player.inventory} {player.curr_xp}')
+            file.close()
+
 
 class load_data():
+    #loads player data from file
     def __init__(self):
-        pass #open file and read data
+        self.data = None
+        with open('game_assets\save.txt','r') as file:
+            self.data = file.read().strip().split(' ')
+            file.close()
+
+    def get_level(self):
+        return int(self.data[0])
+
+    def get_item(self):
+        return self.data[1]
+
+    def get_xp(self):
+        return self.data[2]
+
 
 class construct_player_data():
+    #inputs player data from load and recovers progress
     def __init__(self):
-        #inputs player data from load and recovers progress
-        self.level = self.curr_level()
+        self.data = None
+        self.player = None
         self.moveset = {1 :'punch',
                         2:'claws',
                         3:'teeth',
@@ -38,21 +62,39 @@ class construct_player_data():
                         6:'fire_big'}
 
         self.passives = {1:'poison',
-                         3:'regenerative',
-                         6:'elusive',}
+                         6:'elusive'}
+
+    def construct(self, load):
+        if load:
+            self.data = load_data()
+            level = self.curr_level()
+            self.player =  player(level, self.curr_moveset(), self.curr_passives(level), self.curr_item(), self.curr_xp())
+        else: 
+            self.player =  player()
+        return self.player
 
     def curr_level(self):
-        try:
-            with open('game_assets\save.txt','r') as file:
-                return int(file.read(1))
-        except: return 1
-                
-    def curr_moveset(self):
-        moveset = [self.moveset[self.level-1 if self.level>1 else 1],
-                   self.moveset[self.level] ]
+        return self.data.get_level()
+
+    def curr_moveset(self, level = None):
+        if level ==None:
+            level = self.curr_level()
+        moveset = [self.moveset[level-1 if level>1 else 1],
+                   self.moveset[level] ]
         return moveset
 
-    def curr_passives(self):
-        if self.level <3: return ['poison']
-        elif self.level <6: return ['poison','regenerative']
-        else: return ['poison','regenerative','elusive']
+    def curr_passives(self, level):
+        if level ==None:
+            level = self.curr_level()
+        if level < 3: 
+            return ['poison']
+        elif level < 6: 
+            return ['poison','elusive']
+        else: 
+            return ['poison','elusive',':) nothing'] #got rid of regen on player because pots are available
+
+    def curr_item(self):
+        return int(self.data.get_item())
+
+    def curr_xp(self):
+        return int(self.data.get_xp())
