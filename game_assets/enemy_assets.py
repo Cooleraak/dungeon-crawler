a5667015fc356235b5df75d504ef69c769403b08
Jakub Davidov <36138874@cuni.cz>
2025-01-14 17:49:31 +0100

    Slightly rework the game logic

--- game_assets/enemy_assets.py
+++ game_assets/enemy_assets.py
@@ -2,26 +2,25 @@ class enemy_tiers():
 	def __init__(self):
 		self.basic_enemy_list = ('anaconda','crab','cow','crow','demon_small','goblin')
 		self.elite_enemy_list = ('bear','demon_big','skeleton','angel','spider')
-		self.boss_enemy_list = ('ouroboros','dragon','ophanim','devil')
+		self.boss_enemy_list = ('ouroboros','dragon','ophanim')
 
 class enemy_action_properties():
 	def __init__(self):
 		self.enemy_actions = {
 			'anaconda':['teeth'], #done
 			'crab':['claws'], #done
-			'cow':['teeth','weight'],
-			'crow':['claws'],
-			'demon_small':['claws','teeth','fire'],
-			'goblin':['teeth','claws'], #done
-			'bear':['teeth','claws','weight'],
-			'demon_big':['claws','fire_big'],
-			'skeleton':['punch'],
-			'angel':['fire'],
-			'spider':['teeth'],
+			'cow':['teeth','weight'], #done
+			'crow':['claws'], #done
+			'demon_small':['claws','teeth','fire'], #done
+			'goblin':['claws','teeth'], #done
+			'bear':['claws','teeth','weight'], #done
+			'demon_big':['claws','fire_big'], #done
+			'skeleton':['punch'], #done
+			'angel':['fire'], #done
+			'spider':['teeth'], #done
 			'ouroboros':['teeth'],
-			'dragon':['teeth','claws','fire_big','weight'],
+			'dragon':['claws','teeth','fire_big','weight'], #done
 			'ophanim':['weight','fire_big'], #done
-			'devil':['claws','fire_big'],
 			}
 		self.enemy_passive_actions = {
 			'anaconda':['poison'],
@@ -31,28 +30,26 @@ class enemy_action_properties():
 			'spider':['poison','elusive'],
 			'ouroboros':['regenerative','poison'],
 			'ophanim':['elusive'],
-			'devil':['summoner_devil'],
 			}
 
 class enemy_stat_properties():
 	def __init__(self, level):
-		# 'enemy_name' : (hp, dmg_multiplier),
+		# 'enemy_name' : [hp, dmg_multiplier],
 		self.enemy_stats = {
-			'anaconda':[1500*(level+1)/level, 5*(level)],
-			'crab':[1000*(level+1)/level, 6*(level)],
-			'cow':[2000*(level+1)/level, 3*(level)],
-			'crow':[1500*(level+1)/level, 6*(level)],
-			'demon_small':[1700*(level+1)/level, 6*(level)],
-			'goblin':[1000*(level+1)/level, 5*(level)],
-			'bear':[3000*(level+1)/level, 8*(level)],
-			'demon_big':[2500*(level+1)/level, 9*(level)],
-			'skeleton':[1500*(level+1)/level, 9*(level)],
-			'angel':[1600*(level+1)/level, 8*(level)],
-			'spider':[1700*(level+1)/level, 8*(level)],
-			'ouroboros':[5000*(level+1)/level, 11*(level)],
-			'dragon':[5000*(level+1)/level, 13*(level)],
-			'ophanim':[4000*(level+1)/level, 12*(level)],
-			'devil':[4000*(level+1)/level, 12*(level)],
+			'anaconda':[1500*(level+1)//level, 5*(level)],
+			'crab':[1000*(level+1)//level, 6*(level)],
+			'cow':[2000*(level+1)//level, 3*(level)],
+			'crow':[1500*(level+1)//level, 6*(level)],
+			'demon_small':[1700*(level+1)//level, 6*(level)],
+			'goblin':[1000*(level+1)//level, 5*(level)],
+			'bear':[3000*(level+1)//level, 8*(level)],
+			'demon_big':[2000*(level+1)//level, 9*(level)],
+			'skeleton':[1500*(level+1)//level, 9*(level)],
+			'angel':[1600*(level+1)//level, 8*(level)],
+			'spider':[1700*(level+1)//level, 8*(level)],
+			'ouroboros':[5000*(level+1)//level, 11*(level)],
+			'dragon':[5000*(level+1)//level, 13*(level)],
+			'ophanim':[4000*(level+1)//level, 12*(level)],
 			}
 
 class enemy_attacks():
@@ -65,9 +62,6 @@ class enemy_attacks():
 			'weight':10,
 			'punch':10,
 			'fire_big':20,
-		#misc
-			'summoner_devil':3,
-			
 		# 'passive' : potency
 			'regenerative':15,
 			'elusive':2,
--- game_assets/game_logic.py
+++ game_assets/game_logic.py
@@ -55,160 +55,156 @@ class exploration_logic():
         player.update_xp()
 
 
-class encounter_logic(): 
-    #general functions for battle
-    def __init__(self,player_char, enemy_char):
-        self.MAX_DEPTH = 2
-        self.player_original = player_char
-        self.enemy_original = enemy_char
-        self.player_final = deepcopy(player_char)
-        self.enemy_final = deepcopy(enemy_char)
-        self.player_copy = deepcopy(player_char)
-        self.enemy_copy = deepcopy(enemy_char)
-    
-    def start_battle(self, player_move): 
-        print(self.enemy_final.mp)
-        #player_move = input(f'MP: {self.player_copy.mp}\nChoose action: {self.player_copy.actions}\n')
-        if player_move!='item':
-            self.make_move(self.player_final, player_move, False)    
-        if self.check_for_EOC(False):
-            return [False, self.enemy_final, self.player_final]
-            
-        if self.enemy_original.name in enemy_tiers().boss_enemy_list:
-            best_move = self.best_move(True)
-            print(self.enemy_original.name,' used ', best_move)
-            self.make_move(self.enemy_copy, best_move, False)
-            print(f'you:{self.player_final.hp} HP, enemy: {self.enemy_final.hp} HP\n')
+class encounter_logic():
+    def __init__(self, player_char, enemy_char):
+        self.player_char = player_char
+        self.enemy_char = enemy_char
+        self.MAX_MINIMAX_DEPTH = 4 if enemy_char.name in enemy_tiers().boss_enemy_list else 2
 
-        best_move = self.best_move(False)
-        print(self.enemy_original.name,' used ', best_move)
-        self.make_move(self.enemy_copy, best_move, False)
-        print(f'you:{self.player_final.hp} HP, enemy: {self.enemy_final.hp} HP\n')
+    def start_battle(self, player_move):
+        self.make_move(self.player_char, player_move)
+        
+        if self.enemy_char.name in enemy_tiers().boss_enemy_list:
+            enemy_move = self.best_move(True)
+            self.make_move(self.enemy_char, enemy_move)
             
-        if self.check_for_EOC(False):
-            return [False, self.enemy_final, self.player_final]
-        return [True, self.enemy_final, self.player_final]
-    
+        enemy_move = self.best_move(False)
+
+        self.make_move(self.enemy_char, enemy_move)
+ 
+        if self.check_for_EOC():
+            return [False, self.enemy_char, self.player_char]
+
+        return [True, self.enemy_char, self.player_char]
+
+
+    def make_move(self, actor, move):
+        if actor.type == 'PLAYER':
+            defender = self.enemy_char
+        else:
+            defender = self.player_char
+
+        if move == 'item':
+            if actor.inventory > 0:
+                if actor.hp == actor.max_hp:
+                    pass #to prevent overhealing
+                elif actor.hp + 25*actor.dmg_multiplier <= actor.max_hp:
+                    actor.hp += 25*actor.dmg_multiplier
+                else:
+                    actor.hp = actor.max_hp
+            return self.player_char
+
+        moveset = enemy_attacks().attacks_and_skills
+        #passive start of turn regeneration
+        regen_val = moveset['regenerative'] if 'regenerative' in actor.passives else 0
+        if actor.hp == actor.max_hp:
+            pass #to prevent overhealing
+        elif actor.hp + regen_val <= actor.max_hp:
+            actor.hp += regen_val
+        else:
+            actor.hp = actor.max_hp
+        
+        #passive dmg reduction if elusive
+        elusive_reduction = 3 if 'elusive' in defender.passives else 0
+
+        if move == 'pass':
+            actor.mp += 51
+        else:            
+            mp_cost = 50*(actor.actions.index(move))
+            actor.mp -= mp_cost
+
+            damage = self.calculalte_dmg(actor, move)
+
+            if damage == False:
+                #punish for insufficient mana; -1 for computer eval
+                actor.mp = -1
+                
+            else:
+                defender.hp -= damage - elusive_reduction*moveset['elusive']
+        
+        return self.player_char, self.enemy_char
+
+    def best_move(self, first_action_of_round):
+        player_copy = deepcopy(self.player_char)
+        enemy_copy = deepcopy(self.enemy_char)
+        best_score = -9999999
+        best_move = 'pass'
+
+        for move in self.enemy_char.actions:
+            self.make_move(self.enemy_char, move)
+            print(f'{move} player hp,mp: {self.player_char.hp, self.player_char.mp}:::: enemy {self.enemy_char.hp, self.enemy_char.mp}')
+            score = self.minimax(self.MAX_MINIMAX_DEPTH, False, first_action_of_round)
+            self.player_char.__dict__ = deepcopy(player_copy.__dict__)
+            self.enemy_char.__dict__ = deepcopy(enemy_copy.__dict__)
+            if score >= best_score:
+                best_score = score
+                best_move = move
+        print(f'used {best_move}')
+        
+        return best_move
+
     def calculalte_dmg(self, actor, move):
-        if actor.mp < 0: 
-            if actor == self.enemy_final or actor == self.player_final:
-                print(f'{actor.type}: insufficient mana, nothing happened')
+        if actor.mp < 0:
             return False
-
         poison_dmg = enemy_attacks().attacks_and_skills['poison'] if 'poison' in actor.passives else 0
         dmg_multiplier = actor.dmg_multiplier
         move_dmg = enemy_attacks().attacks_and_skills[move]
         return (move_dmg+poison_dmg) * dmg_multiplier
 
-    def make_move(self,actor, move, is_testing):
-        #makes move for game simulation and for real move
-        curr_player= deepcopy(self.player_copy) if is_testing else self.player_final
-        curr_enemy = deepcopy(self.enemy_copy) if is_testing else self.enemy_final    
-        
-        defender = curr_player if actor==self.enemy_copy else curr_enemy
-
-        actor.mp += 2*actor.dmg_multiplier
-        if actor.mp > 5*actor.dmg_multiplier:
-            actor.mp = 5*actor.dmg_multiplier
-
-        if move == 'item':
-            if actor.inventory != 0:
-                actor.hp += 25*actor.dmg_multiplier
-                return curr_player, curr_enemy 
-            print('insufficient item')
-            return curr_player, curr_enemy
-        if move == 'pass':
-            return curr_player, curr_enemy
-        mp_cost = 10*(actor.actions.index(move)+1)
-        actor.mp -= mp_cost
-
-        moveset = enemy_attacks().attacks_and_skills
-        #passive start of turn regeneration
-        actor.hp += moveset['regenerative'] if 'regenerative' in actor.passives else 0
-        
-        #passive dmg reduction if elusive
-        elusive_reduction = random.randint(2,10) if 'elusive' in defender.passives else 0
-
-        damage = self.calculalte_dmg(actor,move)
-        if damage == False:
-            actor.mp += mp_cost
-            return curr_player, curr_enemy
-        defender.hp -= damage - (elusive_reduction*moveset['elusive'])
-        
-        #returns both characters after acting in case of passive or timer changes
-        return curr_player, curr_enemy
-        
-    def best_move(self, first_action_of_round):
-        #finds theorethical best move for computer
-        enemy_copy = deepcopy(self.enemy_final)
-        max_score = -float('inf')
-        best_move = None
-        for move in enemy_copy.actions:
-            self.player_copy, self.enemy_copy = self.make_move(enemy_copy,move, True)
-            score = self.minimax(self.MAX_DEPTH, False, first_action_of_round)
-            self.player_copy, self.enemy_copy = self.player_final, self.enemy_final
-            if score > max_score:
-                max_score = score
-                best_move = move
-        return best_move
-
-    def minimax(self, curr_depth, enemy_turn, first_action_of_round):
+    def minimax(self, curr_depth, enemy_turn, first_action_of_round): 
         #simulate fight
-        score = self.score_calculator()
-        if self.check_for_EOC(True) == True: 
-            return score*100000
-        if curr_depth == 0:
+        score = self.calculate_score()
+        if self.check_for_EOC():
             return score
-        
-        enemy_copy = deepcopy(self.enemy_copy)
-        player_copy = deepcopy(self.player_copy)
+        if curr_depth < 0:
+            return score
 
-        if enemy_copy.name in enemy_tiers().boss_enemy_list:
+        #enables bosses to attack twice in a row
+        if self.enemy_char.name in enemy_tiers().boss_enemy_list:
             enemy_turn = True if first_action_of_round == True else False
-        first_action_of_round = False if first_action_of_round == True else True
+            first_action_of_round = False if first_action_of_round == True else True
+
+        player_copy = deepcopy(self.player_char.__dict__)
+        enemy_copy = deepcopy(self.enemy_char.__dict__)
 
         if enemy_turn:
-            max_score = -float('inf')
-            for move in enemy_copy.actions:
-                self.player_copy, self.enemy_copy = self.make_move(enemy_copy,move, True)
-                score = self.minimax(curr_depth -1, False, first_action_of_round)
-                self.player_copy,self.enemy_copy = player_copy, enemy_copy
-                if score > max_score:
+            max_score = -9999999
+            for move in self.enemy_char.actions:
+                self.make_move(self.enemy_char, move)
+                score = self.minimax(curr_depth - 1, False, first_action_of_round)
+                self.player_char.__dict__, self.enemy_char.__dict__ = player_copy, enemy_copy
+                if score >= max_score:
                     max_score = score
             return max_score
-                
         else:
-            min_score = float('inf')
-            for move in player_copy.actions:
-                self.player_copy,self.enemy_copy = self.make_move(player_copy, move, True)
-                score = self.minimax(curr_depth -1, True, True)
-                self.player_copy,self.enemy_copy = player_copy, enemy_copy
-                if score < min_score:
+            min_score = 9999999
+            for move in self.player_char.actions:
+                self.make_move(self.player_char, move)
+                score = self.minimax(curr_depth - 1, True, first_action_of_round)
+                self.player_char.__dict__, self.enemy_char.__dict__ = player_copy, enemy_copy
+                if score <= min_score:
                     min_score = score
             return min_score
-                       
-    def score_calculator(self):
-        #calculates minimax score based on resource percentage difference
-        player_hp_percentage = 100*self.player_copy.hp/self.player_original.hp
-        enemy_hp_percentage = 100*self.enemy_copy.hp/self.enemy_original.hp
 
-        player_mp_percentage = 100*self.player_copy.mp/self.player_original.mp
-        if self.enemy_original.mp == 0:
-            self.enemy_original.mp = 1
-        enemy_mp_percentage = 100*self.enemy_copy.mp/self.enemy_original.mp
-        
-        return (player_hp_percentage - enemy_hp_percentage + 
-                player_mp_percentage - enemy_mp_percentage )
 
-    def check_for_EOC(self, is_testing):
+    def calculate_score(self):
+        if self.enemy_char.mp < 0:
+            mana_var = 0
+        else:
+            mana_var = 1
+        if self.player_char.hp < 1:
+            return 9999999
+        elif self.enemy_char.hp < 1:
+            return -9999999
+        else:
+            enemy_hp_percent = (self.enemy_char.hp*100)//self.enemy_char.max_hp
+            player_hp_percent = (self.player_char.hp*100)//self.player_char.max_hp
+            return mana_var*enemy_hp_percent - player_hp_percent
+
+    def check_for_EOC(self):
         #checks for EndOfCombat
-        if is_testing: curr_player, curr_enemy = self.player_copy, self.enemy_copy
-        else: curr_player, curr_enemy = self.player_final, self.enemy_final
-        if curr_player.hp<1 and curr_player == self.player_final: 
-            print('you lose')
-            exit()
-        elif curr_enemy.hp < 1:
+        if self.enemy_char.hp < 1:
             return True
-        elif curr_player == self.player_copy and curr_player.hp < 1:
+        elif self.player_char.hp < 1:
             return True
         return False
