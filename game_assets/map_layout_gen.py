7bb2758a1618bb6aa20936009577ca753655844f
Jakub Davidov <36138874@cuni.cz>
2025-01-14 17:51:27 +0100

    Add portal door

--- game_assets/map_layout_gen.py
+++ game_assets/map_layout_gen.py
@@ -16,7 +16,7 @@ class game_map_generation():
             self.child_right = None
             self.node_type = node_type
             self.parent = parent
-            self.contents = ['enemy','loot']
+            self.contents = ['enemy','loot', 'door']
              
     def __init__(self, goal=None):
         self.MAX_DEPTH = 5
@@ -26,11 +26,14 @@ class game_map_generation():
         self.enemy_count = 0
         self.total_room_count = 0
         self.explored_room_count = 0
+        self.portal_generated = False
 
     def generate_map(self):
         #initiates generation and returns first node
         if self.MAX_DEPTH<1 : return None
         self.root = self.generate_node(1,1)
+        if self.portal_generated == True:
+            self.generate_portal_route()
         return self.root
 
     def generate_node(self, curr_depth, node_data, parent = None):
@@ -40,13 +43,22 @@ class game_map_generation():
         node_type = self.choose_node_type(curr_depth)
         if node_type == 'wall': 
             return None
-        self.total_room_count += 1
+        
+        
         node = self.Node(node_data, node_type, parent)
+        node.contents[2] = self.choose_door_type(node_type)
+        if node_type == 'portal':
+            node.child_left = None
+            node.child_right = None
+            return node
+
         node_data_left = node_data*2
         node_data_right = 1 + node_data*2
 
         node.child_left = self.generate_node(curr_depth + 1, node_data_left, node)
+        
         node.child_right = self.generate_node(curr_depth + 1, node_data_right, node)
+        self.total_room_count += 1
         return node
 
     def choose_node_type(self, curr_depth):
@@ -71,4 +83,64 @@ class game_map_generation():
         if node_type in ['basic_enemy', 'elite_enemy']: 
             self.enemy_count += 1
         
+        if node_type == 'wall' and self.portal_generated==False: 
+            node_type = random.choice(['wall' for _ in range(9)]+['portal'])
+            self.portal_generated = True
         return node_type
+
+    def choose_door_type(self, node_type):
+        door_types = (['trap' for _ in range(4)] +
+                      ['nothing' for _ in range(9)])
+        return random.choice(door_types)
+
+    def generate_portal_route(self):
+        stack = [self.root]
+        portal = None
+        reward = None
+        while len(stack)>0:
+            curr = stack.pop()
+            left = curr.child_left
+            right = curr.child_right
+            if left != None:
+                stack.append(left)
+                if left.node_type == 'portal':
+                    portal = [curr, 'left']
+
+            if right != None:
+                stack.append(right)
+                if right.node_type == 'portal':
+                    portal = [curr, 'right']
+
+
+            if portal != None:
+                break
+
+        if portal == None:
+            return
+
+        stack = [self.root]
+        while len(stack)>0:
+            curr = stack.pop()
+            
+            if curr.node_type == 'reward':
+                reward = curr
+                reward.contents[2] = 'portal'
+                break
+            
+            left = curr.child_left
+            right = curr.child_right
+            if left != None:
+                stack.append(left)
+            if right != None:
+                stack.append(right)
+            
+
+        if portal[1] == 'left':
+            portal[0].child_left = None
+            portal[0].child_left = reward
+        else:
+            portal[0].child_right = None
+            portal[0].child_right = reward
+
+
+
